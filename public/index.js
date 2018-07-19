4fe8619949acba2ae63722cb360c237f33140e4a
Jeffrey Ololu <jeffreyololu@Jeffreys-iMac.local>
2018-07-19 11:43:52 +0200

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,3 @@
+console.log('This is from the public folder', __filename);
+
+console.log('This is from the public folder', __dirname);
---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+//const public = require('./public');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+hbs.registerHelper('getCurrentYear', ()=> {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text)=> {
+    return text.toUpperCase();
+  });
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs'); // It knows when to interpret it
+
+
+app.use((req, res, next) => {
+    //console.log(req);
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+
+    fs.appendFileSync('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+     next();
+});
+
+// app.use((req, res, next) => {
+//    res.render('maintenance.hbs');
+// });
+
+
+app.use(express.static(__dirname + '/public'));
+// console.log("this is the :", __dirname);
+// console.log(public);
+
+
+
+/* req: Is input(request) from the User 
+res: The data base response to the user request */
+
+app.get('/', (req, res) => { 
+    //res.send('<h1>Hello Express</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcome: 'You are welcome to Narture'
+    })
+});
+
+app.get('/about', (req, res) => { 
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+        
+    }) ;
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        message: 'Unable to return message'
+    })
+})
+
+app.listen(port, ()=> {
+    console.log('Server is up on port 3000');
+});
