cbb12a6892ba950d735492b7ea575a3e001eff7c
Jeffrey Ololu <jeffreyololu@Jeffreys-iMac.local>
2018-07-19 23:19:01 +0200

    Setup start script and heroko port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 hbs.registerHelper('getCurrentYear', ()=> {
   return new Date().getFullYear()
@@ -69,5 +69,5 @@ app.get('/bad', (req, res) => {
 })
 
 app.listen(port, ()=> {
-    console.log('Server is up on port 3000');
+    console.log(`Server is up on port ${port}`);
 });
